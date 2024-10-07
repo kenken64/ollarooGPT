37cc84ba072f66f6f1fecc43ebdbc5ff00fb7605
kenken64 <bunnyppl@hotmail.com>
2024-10-07 12:38:57 +0800

    upd

--- ollaro-nextjs/src/middleware.ts
+++ ollaro-nextjs/src/middleware.ts
@@ -3,8 +3,9 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
     console.log(request)
-    return NextResponse.json({ message: 'Auth required' }, { status: 401 })
+    //return NextResponse.json({ message: 'Auth required' }, { status: 401 })
     //return NextResponse.redirect(new URL("/", request.nextUrl));
+    return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more

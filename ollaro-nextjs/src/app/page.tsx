24479557a8646400fbae3147dd6a96075ab1f91a
kenken64 <bunnyppl@hotmail.com>
2025-05-26 14:36:24 +0800

    upd

--- ollaro-nextjs/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h2 className="text-3xl font-bold mb-4">Welcome to My App!</h2>
-      <p>This is the main landing page.</p>
-    </div>
-  );
-}
--- ollaro-nextjs/src/app/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    let mongoose: {
-      Types: any;
-      conn: any | null;
-      promise: Promise<any> | null;
-    };
-  }
-  
-  export {};
